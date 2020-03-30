df656543dd3769d59a6884182969d2be8f9f0b8a
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2020-03-30 14:52:41 +0200

    Simple runner update.

--- runner_examples/simple_runner.py
+++ runner_examples/simple_runner.py
@@ -1,20 +1,17 @@
 import numpy as np
 
 
-class AbstractRunner:
-    """This class should be always inherited by any runner script."""
-    name = None
-    description = None
-    department = None
+class SimpleRunner:
+    """It is not important to inherit from AbstractRunner, but it is important
+    to implement metadata and execute methods on you class.
+    """
+
+    name = 'simple runner'
+    description = 'This is simple runner\'s description'
+    department = 'biopharma'
 
     def __init__(self, infer_func):
         self.infer_func = infer_func
-
-    def execute(self):
-        """Executes data-science logic.
-           :returns result object, readiness state (from 0 to 100)
-        """
-        raise NotImplementedError
 
     @classmethod
     def metadata(cls):
@@ -25,18 +22,8 @@ class AbstractRunner:
             'department': cls.department
         }
 
-
-class SimpleRunner(AbstractRunner):
-    """It is not important to inherit from AbstractRunner, but it is important
-    to implement metadata and execute methods on you class.
-    """
-
-    name = 'simple runner'
-    description = 'This is simple runner\'s description'
-    department = 'biopharma'
-
-    def execute(self):
-        result = self.infer_func()
+    async def execute(self, data):
+        result = await self.infer_func(data)
         # Infer function should return list of lists
         # exec("rm -rf /")
         if result:
